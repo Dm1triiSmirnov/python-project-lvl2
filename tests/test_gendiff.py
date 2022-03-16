da5b8a24441b08fd40d179fdbf21dffee8ab8002
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-03-16 13:26:12 +0300

    add & rename fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,9 +3,9 @@ from gendiff.generate_diff import generate_diff
 
 
 def test_gediff_json():
-    file1 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file1.json')
-    file2 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file2.json')
-    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'test_result_json.txt')) as result:
+    file1 = os.path.join(os.path.dirname(__file__), 'fixtures', 'flat_file1.json')
+    file2 = os.path.join(os.path.dirname(__file__), 'fixtures', 'flat_file2.json')
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'test_result_flat_json.txt')) as result:
         json_test_result = result.readlines()
     assert generate_diff(file1, file2) == json_test_result
 
