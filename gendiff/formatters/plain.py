73376a7c8d09a589ab09ee88332b19ce9822fe3c
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-04-27 16:59:49 +0300

    fix gen_diff.py and plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -3,11 +3,11 @@ import json
 from gendiff.compare_data import CHANGED, ADDED, REMOVED, NESTED
 
 
-TEMPLATES = {
-    ADDED: 'Property \'{0}\' was added with value: {1}',
-    REMOVED: 'Property \'{0}\' was removed',
-    CHANGED: 'Property \'{0}\' was updated. From {1} to {2}'
-}
+ADDED_TEMPLATE = 'Property \'{0}\' was added with value: {1}'
+REMOVED_TEMPLATE = 'Property \'{0}\' was removed'
+CHANGED_TEMPLATE = 'Property \'{0}\' was updated. From {1} to {2}'
+
+COMPLEX_VALUE = '[complex value]'
 
 
 def convert_to_plain(data, name=''):
@@ -21,25 +21,25 @@ def convert_to_plain(data, name=''):
 
         if status == ADDED:
             if isinstance(value, dict):
-                value = '[complex value]'
+                value = COMPLEX_VALUE
             else:
                 value = make_str(value)
-            result.append(TEMPLATES[ADDED].format(path, value))
+            result.append(ADDED_TEMPLATE.format(path, value))
 
         elif status == REMOVED:
-            result.append(TEMPLATES[REMOVED].format(path))
+            result.append(REMOVED_TEMPLATE.format(path))
 
         elif status == CHANGED:
             if isinstance(value.get('old_type'), dict):
-                old = '[complex value]'
+                old = COMPLEX_VALUE
                 new = make_str(value.get('new_type'))
             elif isinstance(value.get('new_type'), dict):
                 old = make_str(value.get('old_type'))
-                new = '[complex value]'
+                new = COMPLEX_VALUE
             else:
                 old = make_str(value.get('old_type'))
                 new = make_str(value.get('new_type'))
-            result.append(TEMPLATES[CHANGED].format(path, old, new))
+            result.append(CHANGED_TEMPLATE.format(path, old, new))
 
         elif status == NESTED:
             if isinstance(children, dict):
--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -1,3 +1,4 @@
+import os
 import json
 import yaml
 
@@ -6,16 +7,16 @@ from .formatters.formatter import formatter, STYLISH
 
 
 def read_file(file_path):
-    file_format = file_path.split('.')[-1]
+    format = os.path.splitext(file_path)[-1].lstrip('.')
     with open(file_path) as file:
         text = file.read()
-        return text, file_format
+        return text, format
 
 
-def parse_file(text, file_format):
-    if file_format == 'json':
+def parse(text, format):
+    if format == 'json':
         parsed_data = json.loads(text)
-    elif file_format == 'yml' or file_format == 'yaml':
+    elif format == 'yml' or format == 'yaml':
         parsed_data = yaml.safe_load(text)
     return parsed_data
 
@@ -23,8 +24,8 @@ def parse_file(text, file_format):
 def generate_diff(file_path1, file_path2, format=STYLISH):
     text1, file_format1 = read_file(file_path1)
     text2, file_format2 = read_file(file_path2)
-    parsed_data1 = parse_file(text1, file_format1)
-    parsed_data2 = parse_file(text2, file_format2)
+    parsed_data1 = parse(text1, file_format1)
+    parsed_data2 = parse(text2, file_format2)
     compared_data = get_diff(parsed_data1, parsed_data2)
     diff = formatter(compared_data, format)
     return diff
